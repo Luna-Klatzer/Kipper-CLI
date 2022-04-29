703a49b7e849553de26b425a31ab60a87403dc4f
Luna-Klatzer <luna.klatzer@gmail.com>
2022-04-29 20:17:17 +0200

    Updated type hints and description in analyse.ts

--- src/commands/analyse.ts
+++ src/commands/analyse.ts
@@ -8,24 +8,24 @@ import {Command, flags} from "@oclif/command";
 import {KipperCompiler} from "@kipper/base";
 import {defaultCliEmitHandler} from "../logger";
 import {KipperLogger} from "@kipper/base/lib/logger";
-import {KipperParseFile} from "../file-stream";
+import {KipperEncodings, KipperParseFile} from "../file-stream";
 
 export default class Analyse extends Command {
-  static description = "Analyses a file and validates its syntax";
+  static description = "Analyses a file and validates its syntax.";
   static examples = [];
 
   static args = [
     {
       name: "file",
       required: true,
-      description: "The file that should be analysed"
+      description: "The file that should be analysed."
     }
   ];
 
   static flags = {
     encoding: flags.string({
       default: "utf8",
-      description: "The encoding that should be used to read the file"
+      description: `The encoding that should be used to read the file (${KipperEncodings.join()}).`
     })
   };
 
@@ -35,14 +35,14 @@ export default class Analyse extends Command {
     const compiler = new KipperCompiler(logger);
 
     // Start timer for processing
-    const startTime = (new Date()).getTime();
+    const startTime: number = (new Date()).getTime();
 
     // Analyse the file
-    const file = await KipperParseFile.fromFile(args.file, flags.encoding as BufferEncoding);
+    const file: KipperParseFile = await KipperParseFile.fromFile(args.file, flags.encoding as BufferEncoding);
     await compiler.syntaxAnalyse(file.stringContent);
 
     // Finished!
-    const endTime = ((new Date().getTime()) - startTime) / 1000;
+    const endTime: number = ((new Date().getTime()) - startTime) / 1000;
     await logger.info(`Finished code analysis in ${endTime}s.`);
   }
 }
