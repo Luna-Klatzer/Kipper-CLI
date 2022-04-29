43421e59d8f6e9c22f6e2d95a401d34fba2dad0a
Luna-Klatzer <luna.klatzer@gmail.com>
2022-04-29 23:20:37 +0200

    Updated compile command to work with `@kipper/base@0.3.0`

--- src/commands/compile.ts
+++ src/commands/compile.ts
@@ -4,13 +4,15 @@
  * @copyright 2021-2022 Luna Klatzer
  * @since 0.0.5
  */
-import { Command, flags } from "@oclif/command";
-import { KipperCompiler } from "@kipper/base";
+import {Command, flags} from "@oclif/command";
+import {KipperCompiler} from "@kipper/base";
 import {KipperLogger} from "@kipper/base/lib/logger";
 import {defaultCliEmitHandler} from "../logger";
+import {KipperEncoding, KipperEncodings, KipperParseFile, verifyEncoding} from "../file-stream";
+import {writeCompilationResult} from "../compile";
 
 export default class Compile extends Command {
-  static description = "Compiles a Kipper source file file into TypeScript";
+  static description = "Compiles a Kipper source file file into TypeScript.";
 
   // TODO! Add examples when the command moves out of development
   static examples = [];
@@ -19,23 +21,40 @@ export default class Compile extends Command {
     {
       name: "file",
       required: true,
-      description: "The file that should be compiled"
+      description: "The file that should be compiled."
     }
   ];
 
   static flags = {
     encoding: flags.string({
-      default: "utf16",
-      description: "The encoding that should be used to read the file"
+      default: "utf8",
+      description: `The encoding that should be used to read the file (${KipperEncodings.join()}).`
+    }),
+    outputDir: flags.string({
+      default: "build",
+      description: `The build directory where the compiled files should be placed. If the path does not exist, it will be created.`
     })
   };
 
   async run() {
-    const { args, flags } = this.parse(Compile);
+    const {args, flags} = this.parse(Compile);
     const logger = new KipperLogger(defaultCliEmitHandler);
     const compiler = new KipperCompiler(logger);
 
-    // TODO! Implement reading of file
-    // await compiler.compile(fileContent, true, flags.encoding as BufferEncoding);
+    // Ensure the encoding is valid
+    verifyEncoding(flags.encoding);
+
+    // Start timer for processing
+    const startTime: number = (new Date()).getTime();
+
+    // Analyse the file
+    const file: KipperParseFile = await KipperParseFile.fromFile(args.file, flags.encoding as KipperEncoding);
+    const result = await compiler.compile(file.stringContent);
+
+    await writeCompilationResult(result, file, flags.outputDir, flags.encoding as KipperEncoding);
+
+    // Finished!
+    const duration: number = ((new Date().getTime()) - startTime) / 1000;
+    await logger.info(`Finished code compilation in ${duration}s.`);
   }
 }
