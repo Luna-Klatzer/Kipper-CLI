e8657812337abdb3640e55f922ebdf72812b68c2
Luna-Klatzer <luna.klatzer@gmail.com>
2022-04-29 23:19:35 +0200

    Added new property `KipperFileStream.path` and added `verifyEncoding()` for encoding support verification

--- src/file-stream.ts
+++ src/file-stream.ts
@@ -7,8 +7,9 @@
  */
 
 import {KipperParseStream} from "@kipper/base"
-import {promises as fs} from "fs";
+import {constants, promises as fs} from "fs";
 import * as path from "path";
+import {KipperFileAccessError, KipperUnsupportedEncodingError} from "./errors";
 
 /**
  * Valid encodings that Kipper supports.
@@ -23,6 +24,21 @@ export type KipperEncoding = 'ascii' | 'utf8' | 'utf16le';
 export const KipperEncodings: Array<KipperEncoding> = ['ascii', 'utf8', 'utf16le'];
 
 /**
+ * Checks whether the encoding is supported. If it is, then it will return it as {@link KipperEncoding}, otherwise it
+ * will throw {@link KipperUnsupportedEncodingError}.
+ * @param encoding
+ * @throws KipperUnsupportedEncodingError If the encoding is not supported by Kipper.
+ */
+export function verifyEncoding(encoding: string): KipperEncoding {
+  const kipperEncoding = KipperEncodings.find(i => i == encoding);
+  if (kipperEncoding) {
+    return kipperEncoding;
+  } else {
+    throw new KipperUnsupportedEncodingError(encoding);
+  }
+}
+
+/**
  * ParserFile class that is used to represent a class that may be given to the
  * compiler to be parsed. This file is a simple wrapper around a file-read and
  * storing as a string in 'stringContent'.
@@ -31,11 +47,13 @@ export const KipperEncodings: Array<KipperEncoding> = ['ascii', 'utf8', 'utf16le
 export class KipperParseFile extends KipperParseStream {
   private readonly _absolutePath: string;
   private readonly _encoding: BufferEncoding;
+  private readonly _path: path.ParsedPath;
 
   constructor(stringContent: string, fileLocation: string, fileName: string, encoding: BufferEncoding) {
     super(stringContent, fileName, fileLocation);
     this._absolutePath = path.resolve(fileLocation);
     this._encoding = encoding;
+    this._path = path.parse(this._absolutePath);
   }
 
   /**
@@ -61,13 +79,21 @@ export class KipperParseFile extends KipperParseStream {
   }
 
   /**
+   * Returns the path as a {@link path.ParsedPath} instance.
+   * @since 0.1.0
+   */
+  get path(): path.ParsedPath {
+    return this._path;
+  }
+
+  /**
    * Creates a new {@link KipperParseFile} instance based on the content of the {@link filePath}
    * @param filePath The path to the file that should be read
    * @param encoding The encoding that should be used to read the file
    * @returns The new {@link KipperParseFile} instance with the set values
    * @since 0.1.0
    */
-  static async fromFile(filePath: string, encoding: string | BufferEncoding = 'utf8'): Promise<KipperParseFile> {
+  static async fromFile(filePath: string, encoding: KipperEncoding = 'utf8'): Promise<KipperParseFile> {
     const fileLocation = path.resolve(filePath);
     const name = (() => {
       const items1 = fileLocation.split("\\");
@@ -77,6 +103,12 @@ export class KipperParseFile extends KipperParseStream {
 
     // Read in the content of the file
     let content: string = (await fs.readFile(fileLocation, encoding as BufferEncoding)).toString();
+
+    try {
+      await fs.access(fileLocation, constants.R_OK);
+    } catch (e) {
+      throw new KipperFileAccessError(fileLocation);
+    }
 
     // Standardising the line endings to '\n'
     content = content.replace(/(\r(\n)?)/gi, "\n");
